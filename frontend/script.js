dc345cae2f35d299a9749f486a0ab173bb8753f3
Dominik Lenda <dlenda1729@gmail.com>
2021-05-20 22:22:20 +0200

    fix bug - production url instead of local url

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
   getVisitCount();
 })
-const functionApiUrl = 'https://getresumecounter-dl.azurewebsites.net/api/GetResumeCounter?code=zaDxNPnIrymFYNwADOHpw4rVsugMX1Nj3c4PTFmvejWQsiKikNKamg=='
+
+const functionApiUrl = 'https://getresumecounteracgdl.azurewebsites.net/api/GetResumeCounter?code=KOrzyAs7Lcp96TjxjSvHLbMxuAcG08WKQby42gUtrGe6L3OhfgqJRA=='
 const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
   let count = 30;
-  fetch(localFunctionApi).then(response => {
+  fetch(functionApiUrl).then(response => {
     return response.json()
   }).then(response => {
     console.log("Website called function API");
