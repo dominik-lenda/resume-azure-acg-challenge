594c68990e38564c5bd13ba0f5e8223c6fd96a86
Dominik Lenda <dlenda1729@gmail.com>
2021-05-22 15:12:34 +0200

    create counter, style it, and test locally

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -5,3 +5,23 @@ AOS.init({
 });
 
 // Add your javascript here
+window.addEventListener('DOMContentLoaded', (event) =>{
+  getVisitCount();
+})
+
+const functionApiUrl = 'https://getresumecounteracgdl.azurewebsites.net/api/GetResumeCounter?code=KOrzyAs7Lcp96TjxjSvHLbMxuAcG08WKQby42gUtrGe6L3OhfgqJRA=='
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const getVisitCount = () => {
+  let count = 30;
+  fetch(localFunctionApi).then(response => {
+    return response.json()
+  }).then(response => {
+    console.log("Website called function API");
+    count = response.count;
+    document.getElementById("counter").innerText = count;
+  }).catch(function(error) {
+    console.log(error);
+  });
+  return count;
+}
